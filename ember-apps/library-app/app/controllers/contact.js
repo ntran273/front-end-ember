634d14507792a13d3a23f255045a5fe8cd4960e0
Nguyen Tran <ntran272@csu.fullerton.edu>
2017-11-13 17:49:07 -0800

    updated

--- ember-apps/library-app/app/controllers/contact.js
+++ ember-apps/library-app/app/controllers/contact.js
@@ -6,21 +6,21 @@ export default Ember.Controller.extend({
   emailAddress: '',
   messages: '',
 
-//Check valid
-isValid: Ember.computed.match('emailAddress', /^.+@.+\..+$/),
-isLength: Ember.computed.gte('messages.length', 5),
-isConditionValid: Ember.computed.and('isValid', 'isLength'),
-isDisabled: Ember.computed.not('isConditionValid'),
+  //Check valid
+  isValid: Ember.computed.match('emailAddress', /^.+@.+\..+$/),
+  isLength: Ember.computed.gte('messages.length', 5),
+  isConditionValid: Ember.computed.and('isValid', 'isLength'),
+  isDisabled: Ember.computed.not('isConditionValid'),
 
 
-//Set click action
-actions:{
+  //Set click action
+  actions: {
 
-  sendMessages(){
-    alert(`Saving Email address in progess: ${this.get('emailAddress')} and messages: ${this.get('messages')}`);
-    this.set('responseMessage',`We got your message and we’ll get in touch soon`);
-    this.set('emailAddress','');
-    this.set('messages','');
+    sendMessages() {
+      alert(`Saving Email address in progess: ${this.get('emailAddress')} and messages: ${this.get('messages')}`);
+      this.set('responseMessage', `We got your message and we’ll get in touch soon`);
+      this.set('emailAddress', '');
+      this.set('messages', '');
+    }
   }
-}
 });
--- ember-apps/library-app/app/controllers/index.js
+++ ember-apps/library-app/app/controllers/index.js
@@ -4,21 +4,21 @@ export default Ember.Controller.extend({
 
   emailAddress: '',
 
-//Check valid
-isValid: Ember.computed.match('emailAddress', /^.+@.+\..+$/),
+  //Check valid
+  isValid: Ember.computed.match('emailAddress', /^.+@.+\..+$/),
 
-//Check whether a property is empty or not
- isDisabled: Ember.computed.empty('emailAddress'),
+  //Check whether a property is empty or not
+  isDisabled: Ember.computed.not('isValid'),
 
 
-//Set click action
-actions:{
+  //Set click action
+  actions: {
 
-  saveInvitation(){
-    alert(`Saving of the following email address in progess: ${this.get('emailAddress')}`);
-    this.set('responseMessage',`Thank you! We've just saved your email address: ${this.get('emailAddress')}`);
-    this.set('emailAddress','');
+    saveInvitation() {
+      alert(`Saving of the following email address in progess: ${this.get('emailAddress')}`);
+      this.set('responseMessage', `Thank you! We've just saved your email address: ${this.get('emailAddress')}`);
+      this.set('emailAddress', '');
+    }
   }
-}
 
 });
